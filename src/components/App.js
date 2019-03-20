7b620d8ea434c1f1b50ddae5933af42d70b41092
Fernando Brandao <fernando.brandao@adarga.ai>
2019-03-20 08:25:05 +0000

    Welcome cmp

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
-
-export default () => {
-    return <div>Hi, im the app</div>;
+export default ({children}) => {
+    return (
+        <div>
+            <Header />
+            {children}
+        </div>
+    );
 };
